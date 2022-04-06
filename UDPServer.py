32a5ff00703f34df7917a49cb24680bbbdc51595
NeXxet <56108498+NeXxet@users.noreply.github.com>
2022-04-06 10:35:02 -0500

    Update UDPServer.py

--- UDPServer.py
+++ UDPServer.py
@@ -1,9 +1,23 @@
 import socket
-serverPort = 12000
-server_socket = socket.socket(socket.AF_INET, socket.SOCKET_DGRAM)
-server_socket.bind(('127.0.0.1', serverPort))
+localIP = "127.0.0.1"
+localPort = 20001
+bufferSize = 1024
+
+msgFromServer = "Hello to client"
+encodedMsg = str.encode(msgFromServer)
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind((localIP, localPort))
 
 while True:
-    data, addr = server_socket.recvfrom(2048)
-    message = bytes("modified message from server").encode('utf-8')
-    server_socket.sendto(message, addr)
+    addressPair = server_socket.recvfrom(bufferSize)
+    data = addressPair[0]
+    address = addressPair[1]
+
+    clientMsg = "Message from Client:{}".format(data)
+    clientIP = "Client IP Address:{}".format(address)
+
+    print(clientMsg)
+    print(clientIP)
+
+    server_socket.sendto(encodedMsg, address)
