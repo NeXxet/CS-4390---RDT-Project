c51ca412df23bf3e53c11705450b1ecf4a369d12
William <willcallihan13@gmail.com>
2022-04-26 13:21:21 -0500

    simplified print statements and added working ACK function using stop and wait functionality

--- UDPClient.py
+++ UDPClient.py
@@ -1,6 +1,8 @@
 import socket
 import sys
 import random
+from time import sleep
+bufferSize = 1024
 
 def ConvertToBin(num, minLength):
     bnry = bin(num).replace('0b','') #convert to binary string and remove prefix
@@ -47,7 +49,14 @@ def BuidHeader(seqNum, ackNum, rcvWin):
 def BuildPacket(header, payload):
     return header + payload
 
-def Corrupt(pkt):
+def Corrupt(pkt, corruptProb):
+    #see if the packet is to be corrupted
+    rand = random.randint(1,100)
+    if rand > corruptProb: #corrupt if the random int is within the corrupt probability
+        return pkt
+
+    #corrupt the packet
+    print("PACKET CORRUPTED")
     index = random.randint(0, len(pkt)-1) #randomly select an index
     if pkt[index] == '0': #flip the selected index
         flipped = '1'
@@ -59,26 +68,31 @@ def Corrupt(pkt):
 def Send(socket, dest, pkt, corruptProb):
     check = MakeChecksum(pkt) #make checksum
     pkt = pkt[:8] + check + pkt[8:] #insert checksum after seq num in packet
-    rand = random.randint(1,100)
-    if rand <= corruptProb: #corrupt if the random int is within the corrupt probability
-        pkt = Corrupt(pkt)
-        print("PACKET CORRUPTED")
-    encodedMsg = str.encode(pkt)
-    socket.sendto(encodedMsg, dest)
-
-CORRUPT_PROBA = int(sys.argv[2]) #corruption probabiity is second argument
-seqNum = 0
+    reply = ''
+    while reply != "ACK":
+        thispkt = Corrupt(pkt, corruptProb) #call the Corrupt method to maybe corrupt the packet
+        encodedMsg = str.encode(thispkt) #encode the new packet
+        socket.sendto(encodedMsg, dest) #send to server
+        pair = socket.recvfrom(bufferSize) #receive a message from the server
+        reply = "{}".format(pair[0]).replace('b', '').replace('\'', '') #get the raw message received
+        print("reply:", reply)
 
 file = open(sys.argv[1], 'r') #data file name is first argument
 data = file.read() #read in all the data to one large string
 file.close() #close the file
+binData = ''.join(format(ord(x), 'b') for x in data) #convert the data to binary
 
+#create socket
 serverPort = ("127.0.0.1", 20001)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-binData = ''.join(format(ord(x), 'b') for x in data) #convert the data to binary
+#set up local variables
+CORRUPT_PROBA = int(sys.argv[2]) #corruption probabiity is second argument
+seqNum = 0
 i = 0
 j = 0
+
+#send the data until it is gone
 while i < len(binData): #iterate through the data, taking 100 bytes each time
     if(len(binData[i:]) >= 800): #if the data has more than 100 bytes left, take the next 100 bytes
         payload = binData[i:i+800]
@@ -94,12 +108,6 @@ while i < len(binData): #iterate through the data, taking 100 bytes each time
     i += 800
     j += 1
 
+#end transfer and close socket
 print("transfer done")
-
-
-bufferSize = 1024
-msgFromServer = client_socket.recvfrom(bufferSize)
-data = "Message from Server {}".format(msgFromServer[0])
-print(data)
-
 client_socket.close()
--- UDPServer.py
+++ UDPServer.py
@@ -30,41 +30,39 @@ def MakeChecksum(pkt): #this is a copy of client's MakeChecksum
             checksum += '1'
     return checksum
 
+#setup local variables
 localIP = "127.0.0.1"
 localPort = 20001
 bufferSize = 1024
 
-msgFromServer = "Hello to client"
-encodedMsg = str.encode(msgFromServer)
-
+#create and bind the socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind((localIP, localPort))
 
+#always be looking for messages
 i = -1
 while True:
-    addressPair = server_socket.recvfrom(bufferSize)
+    #receive and acknowledge all received messages and increment i once a correct packet is received
+    correctPkt = False
     i += 1
-    packet = addressPair[0]
-    address = addressPair[1]
+    while correctPkt == False:
+        addressPair = server_socket.recvfrom(bufferSize)
+        packet = addressPair[0]
+        address = addressPair[1]
 
-    seqNum = "{}".format(packet[:8]).replace('b', '').replace('\'', '') #sequence number is first 8 bits
-    checksum = "{}".format(packet[8:24]).replace('b', '').replace('\'', '') #checksum is the next 16 bits
-    payload = "{}".format(packet[24:]).replace('b', '').replace('\'', '') #payload is the rest
-    #IMPORTANT: change these assignments according to how the packet is
-    # generated in the UDPClient file, there is no way to have the server
-    # check for us
-    
-    rcvrChecksum = MakeChecksum(seqNum + payload) #make receiver checksum
-    if checksum == rcvrChecksum:
-        print(i, "Correct packet received")
-    else:
-        print(i, "Incorrect packet received")
-
-    #clientMsg = "Message from Client:{}".format(data)
-    #clientIP = "Client IP Address:{}".format(address)
-
-    #print(clientMsg)
-    #print(clientIP)
-    #print(i)
-
-    #server_socket.sendto(encodedMsg, address)
+        seqNum = "{}".format(packet[:8]).replace('b', '').replace('\'', '') #sequence number is first 8 bits
+        checksum = "{}".format(packet[8:24]).replace('b', '').replace('\'', '') #checksum is the next 16 bits
+        payload = "{}".format(packet[24:]).replace('b', '').replace('\'', '') #payload is the rest
+        #IMPORTANT: change these assignments according to how the packet is
+        # generated in the UDPClient file, there is no way to have the server
+        # check for us
+        
+        rcvrChecksum = MakeChecksum(seqNum + payload) #make receiver checksum
+        if checksum == rcvrChecksum:
+            print(i, "Correct packet received")
+            server_socket.sendto(str.encode("ACK"), address)
+            correctPkt = True
+        else:
+            print(i, "Incorrect packet received")
+            server_socket.sendto(str.encode("NACK"), address)
+            correctPkt = False
